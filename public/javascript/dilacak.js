6ad05bd2aef4dbbf8f5e68952c143b5a839270a9
aviandri <aviandri@gmail.com>
2014-02-18 16:23:37 +0700

    fix close nav on click

--- public/javascript/dilacak.js
+++ public/javascript/dilacak.js
@@ -1,20 +1,4 @@
 
-function navToggle (argument) {
-	var navigation = responsiveNav(".nav-collapse", {customToggle: ".nav-toggle",
-	animate: true,        // Boolean: Use CSS3 transitions, true or false
-	transition: 50,      // Integer: Speed of the transition, in milliseconds
-	open: function(){
-		$(".navigator").css("background-color", "#333");
-	}, 
-	close: function(){
-		if($('body').find('.navigator.active').length = 1){
-			$(".navigator").css("background-color", "#4980a5");
-		} else{
-			$(".navigator").css("background-color", "transparant");
-		}
-	}
-	});
-}
 
 
 function scrollToAnchor(aid){
@@ -31,6 +15,29 @@ $(document).ready(function() {
 	$(document).on('click', '.close', function(){
 		$(".statusbar .container .content").remove();
 	});
+
+
+	var navigation = responsiveNav(".nav-collapse", {customToggle: ".nav-toggle",
+	animate: true,        // Boolean: Use CSS3 transitions, true or false
+	transition: 50,      // Integer: Speed of the transition, in milliseconds
+	open: function(){
+		$(".navigator").css("background-color", "#333");
+	}, 
+	close: function(){
+		if($('body').find('.navigator.active').length = 1){
+			$(".navigator").css("background-color", "#4980a5");
+		} else{
+			$(".navigator").css("background-color", "transparant");
+		}
+	}
+	});
+
+	$('.nav-collapse ul li a').on('click',function(){			
+		if($(".nav-collapse.opened").length > 0){
+    		navigation.toggle();
+    	}
+	});
+
 }
 );
 
